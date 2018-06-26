fae168e9583b7d6e8802df0432c11800f2833db3
Halil Ozan Akgül <hozanakgul@gmail.com>
2018-06-26 14:18:32 +0200

    Neighbour cities added

--- code.py
+++ code.py
@@ -1,71 +1,90 @@
 import csv
 import math
 
-T = []
 Tt = []
-date = []
+
+data = {}
+
 
 with open("data.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter = ',', quotechar='"')
     for row in reader:
         if(row[0]!='STATION'):
-            T.append(float(row[3]))
-            date.append(row[2])
+            if not(row[2] in data):
+                data[row[2]] = [float('nan'), float('nan'), float('nan'), float('nan')]
+            if row[1] == "MUGLA, TU":
+                data[row[2]][0] = float(row[3])
+            if row[1] == "BALIKESIR, TU":
+                data[row[2]][1] = float(row[3])
+            if row[1] == "AYDIN, TU":
+                data[row[2]][2] = float(row[3])
+            if row[1] == "IZMIR GUZELYALI, TU":
+                data[row[2]][3] = float(row[3])
 
-#t = n0 + n1*t1 + n2*t2 + n3*t3 + n4*t4 + n5*t5
+sortedData = sorted(data.items(), key = lambda kv: kv[0])
 
-n = [0.75, 1, 1, 1, 1, 1]
+sortedData = sortedData[8430:]
+
+for row in range(len(sortedData)):
+    for i in range(4):
+        if math.isnan(sortedData[row][1][i]):
+            sortedData[row][1][i] = round(sortedData[row-1][1][i]*0.5 + sortedData[row-1][1][i]*0.25 + sortedData[row-1][1][i]*0.15 + sortedData[row-1][1][i]*0.1, 1)
+
+T = [row[1] for row in sortedData]
+
+#t = n0 + n1*t1 + n2*t2 + n3*t3 + n4*t4 + n5*t5 + n6*t6
+
+n = [0.75, 1, 1, 1, 1, 1, 1]
 
 N = len(T)
 Nt = int(N/10)
 N -= Nt
 
-T = [x/25 for x in T]
+T = [[x[0]/25, x[1]/25, x[2]/25, x[3]/25] for x in T]
 
 alpha = 0.0001
 
 def calcMSE():
     ae = 0
     for i in range(5, N):
-        ae += ((T[i] - (n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5]))**2)/N
+        ae += ((T[i][3] - (n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2]))**2)/N
     return math.sqrt(ae)
 
 def calDer(x):
     res = 0
-    for i in range(5,N):
-        res += -T[i-x]*(T[i]-(n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5]))
-    return res;
-
-def calDer1():
-    res = 0
-    for i in range(5,N):
-        res += -(T[i]-(n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5]))
+    if x == 0:
+        for i in range(5,N):
+            res += -(T[i][3] - (n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2]))
+    if x <= 3 and x >= 1:
+        for i in range(5,N):
+            res += -T[i-x][3]*(T[i][3]-(n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2]))
+    if x > 3:
+        for i in range(5,N):
+            res += -T[i-1][x-4]*(T[i][3]-(n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2]))
     return res;
 
 def calError():
     ave = 0
     for i in range(N,N+Nt):
-        ave += abs(T[i] - (n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5]))/Nt
+        ave += abs((T[i][3] - (n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2])))/Nt
         print(
-            "%.2f %.3f" % (T[i], (n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5])),
-            T[i] - (n[0] + n[1]*T[i-1] + n[2]*T[i-2] + n[3]*T[i-3] + n[4]*T[i-4] + n[5]*T[i-5])
+            "%.2f %.3f" % (T[i][3], (n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2])),
+            T[i][3] - (n[0] + n[1]*T[i-1][3] + n[2]*T[i-2][3] + n[3]*T[i-3][3] + n[4]*T[i-1][0] + n[5]*T[i-1][1] + n[6]*T[i-1][2])
         )
     print(ave)
-
 """
 while abs(calDer(1))>0.01:
-    nn = [0, 0, 0, 0, 0, 0]
-    nn[0] = n[0] - alpha*calDer1()
-    for j in range(1, 6):
+    nn = [0, 0, 0, 0, 0, 0, 0]
+    for j in range(7):
         nn[j] = n[j] - alpha*calDer(j)
 
     n = nn
-    #print(n, calcMSE())
+    print(n, calcMSE())
 """
 
-n = [0.025721993180673964, 0.7295744826618324, -0.0672309715502604, 0.11744543302665661, 0.044112288076320394, 0.12862599813770678]
-n[0] *= 25
-T = [x*25 for x in T]
+n = [1.3222737946806093, 1.014403495152182, -0.4245195209884346, 0.17905047145596262, 0.15448111463168568, 0.0373477100009339, -0.005824989667334112]
+#n[0] *= 25
+T = [[x[0]*25, x[1]*25, x[2]*25, x[3]*25] for x in T]
 
-calError()
 print(n)
+calError()
